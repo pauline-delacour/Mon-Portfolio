ab49d385846f68f09b7eae9cbd0a996fe5a091b7
Pauline Delacour <pauline.delacour33@gmail.com>
2024-11-09 18:39:31 +0100

    Modification min-width media queries + ajout de mot en gras page About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const About = () => {
+  //Permet de s'assurer que la page soit en position haut de page
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []); // Tableau vide , L'effet s'exécute une seule fois lors du chargement de la page du projet
   return (
     <article className="apropos">
       <div>
@@ -8,7 +12,7 @@ const About = () => {
         <p>Moi, c'est Pauline ! </p>
         <p>
           J'ai 29 ans et suis actuellement en reconversion professionnelle vers
-          le développement web grâce à une formation d'OpenClassrooms.
+          le <b>développement web</b> grâce à une formation d'OpenClassrooms.
         </p>
 
         <p>
@@ -25,7 +29,7 @@ const About = () => {
           Cette expérience m'a sensibilisée à la manière dont chaque action
           individuelle contribue à la réussite de l'ensemble des défis. Cela a
           éveillé mon intérêt pour les processus et leurs impacts directs sur le
-          travail en équipe.
+          <b> travail en équipe</b>.
         </p>
         <p>
           Curieuse de nature, j’aime comprendre ce que je fais et en saisir les
@@ -37,17 +41,17 @@ const About = () => {
           Animée par un besoin constant d'apprendre de nouvelles choses et de
           repousser mes compétences, je me suis tournée vers le développement
           web pour construire des projets uniques et captivants. Au cours de ma
-          formation, j'ai acquis des compétences en HTML, CSS, JavaScript, et
-          React, des technologies que je suis impatiente d’approfondir au sein
-          d’une entreprise en alternance. Je me suis également découvert un fort
-          attrait pour le UX design et le maquettage sur Figma. Pour moi,
+          formation, j'ai acquis des compétences en <b>HTML, CSS, JavaScript, et
+          React</b>, des technologies que je suis impatiente d’approfondir au sein
+          d’une entreprise en <b>alternance</b>. Je me suis également découvert un fort
+          attrait pour le <b>UX design</b> et <b>le maquettage sur Figma</b>. Pour moi,
           l'expérience en entreprise est la meilleure façon de progresser, en
           appliquant et consolidant mes connaissances.
         </p>
         <p>
           En dehors du développement, je suis passionnée de couture : j’adore
           transformer un simple morceau de tissu en quelque chose d'unique.
-          C'est cette dynamique de création qui me motive jour après jour !
+          C'est cette dynamique de <b>création</b> qui me motive jour après jour !
         </p>
 
         <p>Merci d'avoir pris le temps de lire ma présentation !</p>
